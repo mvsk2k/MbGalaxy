a0f76e09f6bdf7931272203f9b68bf2cb9121f6f
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-12-31 19:32:39 +0530

    Perspective Transform

--- main.py
+++ main.py
@@ -62,9 +62,38 @@ class MainWidget(Widget):
 
         for i in range(0, self.V_NB_LINES):
             line_x = int(center_line_x + offset * spacing)
-            self.vertical_lines[i].points = [line_x, 0, line_x, self.height]
+
+            x1, y1 = self.transform(line_x, 0)
+            x2, y2 = self.transform(line_x, self.height)
+
+            #self.vertical_lines[i].points = [line_x, 0, line_x, self.height]
+            self.vertical_lines[i].points = [x1, y1, x2, y2]
             offset += 1
 
+
+    def transform(self, x, y):
+
+        #return self.transform_2D(x, y)
+        return self.transform_perspective(x, y)
+
+
+    def transform_2D(self, x, y):
+        return int(x), int(y)
+
+
+    def transform_perspective(self, x, y):
+        tr_y = y * self.perspective_point_y /self.height
+
+        if tr_y > self.perspective_point_y:
+            tr_y = self.perspective_point_y
+
+        diff_x = x-self.perspective_point_x
+        diff_y = self.perspective_point_y - tr_y
+        proportion_y = diff_y / self.perspective_point_y  #1 when diff_y == self.perspective_point_y / 0 when diff_y == 0
+
+        tr_x = self.perspective_point_x + diff_x * proportion_y
+
+        return int(tr_x), int(tr_y)
 
 
 
