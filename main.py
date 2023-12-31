b6dee41d74067b973d1a449aeeeddded963b2c74
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-12-31 11:47:34 +0530

    perspectivepoint

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ class MainWidget(Widget):
 
     def on_size(self, *args):
         print(" On Size " + str(self.width) + " H " + str(self.height))
+        self.perspective_point_x = self.width/2
+        self.perspective_point_y = self.height * 0.75
+
+    def on_perspective_point_x(self, widget, value):
+        print("PX " + str(value))
+
+    def on_perspective_point_y(self, widget, value):
+        print("PY " + str(value))
+
+
+
 
 
 
