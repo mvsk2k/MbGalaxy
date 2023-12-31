e4d9a6c240e83b8e2d52e7e7c0052877a4b19638
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-12-31 12:59:24 +0530

    verticalLines

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
-    line = None
+    #line = None
+
+    V_NB_LINES = 7
+    V_LINES_SPACING = 0.1  #Percentage in screen width
+    vertical_lines = []
+
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -43,11 +48,22 @@ class MainWidget(Widget):
     def init_vertical_lines(self):
         with self.canvas:
             Color(1, 1, 1)
-            self.line = Line(points=[100, 0, 100, 100])
+            #self.line = Line(points=[100, 0, 100, 100])
+            for i in range(0, self.V_NB_LINES):
+                self.vertical_lines.append(Line())
 
     def update_vertical_lines(self):
-        center_x = int(self.width/2)
-        self.line.points = [center_x, 0, center_x, 100]
+        #center_x = int(self.width/2)
+        #self.line.points = [center_x, 0, center_x, 100]
+
+        center_line_x = int(self.width / 2)
+        spacing = self.V_LINES_SPACING * self.width
+        offset = -int(self.V_NB_LINES/2)
+
+        for i in range(0, self.V_NB_LINES):
+            line_x = int(center_line_x + offset * spacing)
+            self.vertical_lines[i].points = [line_x, 0, line_x, self.height]
+            offset += 1
 
 
 
