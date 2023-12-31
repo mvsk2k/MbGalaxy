f88b436c95cc0c4f07a34aa26964912219ceca1c
M V SIVAKUMAR <mvsk2k@gmail.com>
2023-12-31 20:44:45 +0530

    HorizontalLines perspective

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ class MainWidget(Widget):
     #line = None
 
     V_NB_LINES = 10
-    V_LINES_SPACING = 0.1  #Percentage in screen width
+    V_LINES_SPACING = 0.25  #Percentage in screen width
     vertical_lines = []
 
     H_NB_LINES = 15
-    H_LINES_SPACING = 0.2  # Percentage in screen Height
+    H_LINES_SPACING = 0.1  # Percentage in screen Height
     horizontal_lines = []
 
 
@@ -112,16 +112,19 @@ class MainWidget(Widget):
 
 
     def transform_perspective(self, x, y):
-        tr_y = y * self.perspective_point_y /self.height
+        lin_y = y * self.perspective_point_y /self.height
 
-        if tr_y > self.perspective_point_y:
-            tr_y = self.perspective_point_y
+        if lin_y > self.perspective_point_y:
+            lin_y = self.perspective_point_y
 
         diff_x = x-self.perspective_point_x
-        diff_y = self.perspective_point_y - tr_y
-        proportion_y = diff_y / self.perspective_point_y  #1 when diff_y == self.perspective_point_y / 0 when diff_y == 0
+        diff_y = self.perspective_point_y - lin_y
+        factor_y = diff_y / self.perspective_point_y  #1 when diff_y == self.perspective_point_y / 0 when diff_y == 0
+        #factor_y = factor_y * factor_y  # same as the next line
+        factor_y = pow(factor_y, 4)
 
-        tr_x = self.perspective_point_x + diff_x * proportion_y
+        tr_x = self.perspective_point_x + diff_x * factor_y
+        tr_y = (1 - factor_y) * self.perspective_point_y
 
         return int(tr_x), int(tr_y)
 
