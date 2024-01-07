ca92e73ac138c2740761a013328bfa4b29c08e3c
M V SIVAKUMAR <mvsk2k@gmail.com>
2024-01-07 19:57:05 +0530

    lost

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import random
 
 from kivy.config import Config
 Config.set('graphics', 'width', '900')
 Config.set('graphics', 'height', '400')
-
+import random
 from kivy import platform
 from kivy.core.window import Window
 
 from kivy.app import App
-from kivy.graphics import Color, Line, Quad
+from kivy.graphics import Color, Line, Quad, Triangle
 from kivy.properties import NumericProperty, Clock
 from kivy.uix.widget import Widget
 
@@ -42,6 +41,11 @@ class MainWidget(Widget):
     tiles = []
     tiles_coordinates = []
 
+    SHIP_WIDTH = 0.1
+    SHIP_HEIGHT = 0.035
+    SHIP_BASE_Y = 0.04
+    ship = None
+
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -50,6 +54,8 @@ class MainWidget(Widget):
         self.init_vertical_lines()
         self.init_horizontal_lines()
         self.init_tiles()
+        self.init_ship()
+        self.pre_fill_tiles_coordinates()
         self.generate_tiles_coordinates()
 
         if self.is_desktop():
@@ -64,12 +70,40 @@ class MainWidget(Widget):
             return True
         return False
 
+    def init_ship(self):
+        with self.canvas:
+            Color(0, 0, 0)
+            self.ship = Triangle()
+
+    def update_ship(self):
+        center_x = self.width/2
+        base_y = self.SHIP_BASE_Y * self.height
+        ship_half_width = self.SHIP_WIDTH * self.width/2
+        ship_height = self.SHIP_HEIGHT * self.height
+        #
+        #      2
+        #  1       3
+        # self.transform
+        x1, y1 = self.transform(center_x - ship_half_width, base_y)
+        x2, y2 = self.transform(center_x, base_y + ship_height)
+        x3, y3 = self.transform(center_x - ship_half_width, base_y)
+
+        self.ship.points = [x1, y1, x2, y2, x3, y3]
+
+
 
     def init_tiles(self):
         with self.canvas:
             Color(1, 1, 1)
             for i in range(0,  self.NB_TILES):
                 self.tiles.append(Quad())
+
+
+    def pre_fill_tiles_coordinates(self):
+        # 10 tiles in a straight line
+        for i in range (0, 10):
+            self.tiles_coordinates.append((0, 1))
+
 
     def generate_tiles_coordinates(self):
         last_x = 0
@@ -86,6 +120,8 @@ class MainWidget(Widget):
             last_coordinates = self.tiles_coordinates[-1]
             last_x = last_coordinates[0]
             last_y = last_coordinates[1] + 1
+
+        print("foo1")
 
         for i in range(len(self.tiles_coordinates), self.NB_TILES):
             r = random.randint(0, 2)
@@ -211,6 +247,7 @@ class MainWidget(Widget):
         self.update_vertical_lines()
         self.update_horizontal_lines()
         self.update_tiles()
+        self.update_ship()
         self.current_offset_y += self.SPEED * time_factor
 
         spacing_y = self.H_LINES_SPACING * self.height
